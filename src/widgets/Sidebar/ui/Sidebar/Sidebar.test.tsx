257976659a3db14d6f0a0cc2c0e915f319ffeaca
Picolina2019 <j.sakowicz91@gmail.com>
2023-01-13 00:14:30 +0100

    loki fix

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,14 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import React from 'react';
-import { renderWithTranslation } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from '../../../../shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('sidebar', () => {
   test('should render', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
   test('should toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
