2da587e937699cac9d9fee77dc39ec5e202de6e2
Picolina2019 <j.sakowicz91@gmail.com>
2023-02-15 23:11:27 +0100

    tests added

---
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -0,0 +1,24 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from '../types/LoginSchema';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice', () => {
+  test('test set username', () => {
+    const state: DeepPartial<LoginSchema> = {
+      username: 'hello',
+    };
+
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setUsername('Katia'))
+    ).toEqual({ username: 'Katia' });
+  });
+  test('test set password', () => {
+    const state: DeepPartial<LoginSchema> = {
+      password: '123',
+    };
+
+    expect(
+      loginReducer(state as LoginSchema, loginActions.setPassword('321'))
+    ).toEqual({ password: '321' });
+  });
+});
