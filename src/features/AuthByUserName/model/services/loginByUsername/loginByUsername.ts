2d963f9f9b0a50fe11821f15da831016753eab25
Picolina2019 <j.sakowicz91@gmail.com>
2023-02-15 23:03:48 +0100

    tests added

--- src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.ts
+++ src/features/AuthByUserName/model/services/loginByUsername/loginByUsername.ts
@@ -7,20 +7,28 @@ interface LoginByUsernameProps {
   username: string;
   password: string;
 }
+
 export const loginByUsername = createAsyncThunk<
   User,
   LoginByUsernameProps,
   { rejectValue: string }
->('login/loginByUsername', async (authData: LoginByUsernameProps, thunkAPI) => {
+>('login/loginByUsername', async (authData, thunkAPI) => {
   try {
-    const response = await axios.post('http://localhost:8000/login', authData);
+    const response = await axios.post<User>(
+      'http://localhost:8000/login',
+      authData
+    );
+
     if (!response.data) {
       throw new Error();
     }
+
     localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data));
     thunkAPI.dispatch(userActions.setAuthData(response.data));
+
+    return response.data;
   } catch (e) {
     console.log(e);
-    return thunkAPI.rejectWithValue('Wrong username or password');
+    return thunkAPI.rejectWithValue('Wrong email or password');
   }
 });
