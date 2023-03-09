20646fe34ca227f926d9f82c0a303fe2a060e046
Picolina2019 <j.sakowicz91@gmail.com>
2023-03-09 23:58:25 +0100

    articles view

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,97 +1,66 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
-import { Article, ArticleList, ArticleView } from 'entities/Article';
-import { memo } from 'react';
+import {
+  Article,
+  ArticleList,
+  ArticleView,
+  ArticleViewSelector,
+} from 'entities/Article';
+import { memo, useCallback } from 'react';
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import {
+  articlesPageActions,
+  articlesPageReducer,
+  getArticles,
+} from 'pages/ArticlesPage/model/slices/articlesPageSlice';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { fetchArticlesList } from 'pages/ArticlesPage/model/services/fetchArticlesList/fetchArticlesList';
+import { useSelector } from 'react-redux';
+import {
+  getArticlesPageError,
+  getArticlesPageIsLoading,
+  getArticlesPageView,
+} from 'pages/ArticlesPage/model/selectors/articlesPageSelectors';
 import styles from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
   className?: string;
 }
-const articles = {
-  id: '1',
-  title: 'Javascript news',
-  subtitle: 'hello world?',
-  img: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
-  views: 1022,
-  createdAt: '26.02.2022',
-  user: {
-    id: '1',
-    username: 'admin',
-    avatar:
-      'https://dl.memuplay.com/new_market/img/com.vicman.newprofilepic.icon.2022-06-07-21-33-07.png',
-  },
-  type: ['IT'],
-  blocks: [
-    {
-      id: '1',
-      type: 'TEXT',
-      title: 'Head',
-      paragraphs: [
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
-      ],
-    },
-    {
-      id: '4',
-      type: 'CODE',
-      code: '<!DOCTYPE html>\n<html>\n  <body>\n    <p id="hello"></p>\n\n    <script>\n      document.getElementById("hello").innerHTML = "Hello, world!";\n    </script>\n  </body>\n</html>;',
-    },
-    {
-      id: '5',
-      type: 'TEXT',
-      title: 'Head 2',
-      paragraphs: [
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.",
-      ],
-    },
-    {
-      id: '2',
-      type: 'IMAGE',
-      src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
-      title: 'Picture',
-    },
-    {
-      id: '3',
-      type: 'CODE',
-      code: "const path = require('path');\n\nconst server = jsonServer.create();\n\nconst router = jsonServer.router(path.resolve(__dirname, 'db.json'));\n\nserver.use(jsonServer.defaults({}));\nserver.use(jsonServer.bodyParser);",
-    },
-    {
-      id: '7',
-      type: 'TEXT',
-      title: 'Head 3',
-      paragraphs: [
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
-      ],
-    },
-    {
-      id: '8',
-      type: 'IMAGE',
-      src: 'https://hsto.org/r/w1560/getpro/habr/post_images/d56/a02/ffc/d56a02ffc62949b42904ca00c63d8cc1.png',
-      title: 'Picture',
-    },
-    {
-      id: '9',
-      type: 'TEXT',
-      title: 'Heading',
-      paragraphs: [
-        "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
-      ],
-    },
-  ],
-} as Article;
+
 const ArticlesPage = (props: ArticlesPageProps) => {
   const { className } = props;
   const { t } = useTranslation();
+  const dispatch = useAppDispatch();
+  const articles = useSelector(getArticles.selectAll);
+  const isLoading = useSelector(getArticlesPageIsLoading);
+  const view = useSelector(getArticlesPageView);
+  const error = useSelector(getArticlesPageError);
 
+  useInitialEffect(() => {
+    dispatch(fetchArticlesList());
+    dispatch(articlesPageActions.initState());
+  });
+
+  const onChangeView = useCallback(
+    (view: ArticleView) => {
+      dispatch(articlesPageActions.setView(view));
+    },
+    [dispatch]
+  );
+  const reducers: ReducersList = {
+    articlesPage: articlesPageReducer,
+  };
   return (
-    <div className={classNames(styles.ArticlesPage, {}, [className])}>
-      <ArticleList
-        view={ArticleView.SMALL}
-        articles={new Array(16).fill(0).map((item, index) => ({
-          ...articles,
-          id: String(index),
-        }))}
-      />
-    </div>
+    <DynamicModuleLoader reducers={reducers}>
+      <div className={classNames(styles.ArticlesPage, {}, [className])}>
+        <ArticleViewSelector view={view} onViewClick={onChangeView} />
+        <ArticleList isLoading={isLoading} view={view} articles={articles} />
+      </div>
+    </DynamicModuleLoader>
   );
 };
 
