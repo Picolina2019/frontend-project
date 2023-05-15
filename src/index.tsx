26ffdb001fc973ac3fca71aab3a4b6c21738a9ae
Picolina2019 <j.sakowicz91@gmail.com>
2023-05-15 23:47:04 +0200

    prettier

--- src/index.tsx
+++ src/index.tsx
@@ -11,19 +11,19 @@ import { ErrorBoundary } from './app/providers/ErrorBoundary';
 const container = document.getElementById('root');
 
 if (!container) {
-  throw new Error('Root is not found');
+    throw new Error('Root is not found');
 }
 
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <StoreProvider>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </StoreProvider>
-  </BrowserRouter>,
+    <BrowserRouter>
+        <StoreProvider>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </StoreProvider>
+    </BrowserRouter>,
 );
